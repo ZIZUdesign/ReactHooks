360943e84247ffb3611b50ecf6c738d4c3fe07ae
Zed <kahsayzeray@gmail.com>
2021-10-15 22:16:02 +0200

    isLoggedIn & onLogout props replaced bt ctx

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
@@ -16,12 +16,12 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <a href="/">Admin</a>
+            <a href="/">Admin</a> 
           </li>
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
